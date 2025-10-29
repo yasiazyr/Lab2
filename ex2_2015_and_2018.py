2c0c42a5cb86878fe7d260ef65be62b060a0eb10
Ярослава <iaroslava.zyrianova@yandex.ru>
2025-10-29 19:20:52 +0300

    Update ex2_2015_and_2018.py

--- ex2_2015_and_2018.py
+++ ex2_2015_and_2018.py
@@ -14,5 +14,8 @@ for r in books:
     if author_name in str(r[author]) and (year == 2015 or year == 2018):
         author_list.append(r)
 
+if len(author_list)==0:
+    print('Такого автора нет')
+
 for s in author_list:
     print(s[author], s[name], s[date][:-5])
